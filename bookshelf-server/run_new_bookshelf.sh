845b6bf062ea209dbdb56887917f5fe8a1511b03
Bernd Fallert <bernd.fallert@bib.uni-mannheim.de>
2018-10-19 11:09:40 +0200

    add script for crontab

---
+++ bookshelf-server/run_new_bookshelf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#script ohne parameter starten
+perl HoleCSV_von_alma.pl
+
+#script mit parameter für ebooks starten
+perl HoleCSV_von_alma.pl --ebooks
+
+#createFiles starten
+perl CreateQRCodeFuerBuecherregal.pl
+perl CreateGesamtBuecherregal.pl
+
+cd html/js
+
+if [ -f ErzeugeMiniVersion.sh ]
+then
+   source ErzeugeMiniVersion.sh
+fi
