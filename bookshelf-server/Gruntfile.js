58a447954b972b3ff0035b5880b3520c927f5eb1
BFallert <bernd.fallert@bib.uni-mannheim.de>
2021-06-21 10:14:55 +0200

    first step to grunt and less

---
+++ bookshelf-server/Gruntfile.js
@@ -0,0 +1,257 @@
+// Generated on 2014-02-03 using generator-webapp 0.4.7
+'use strict';
+
+// ggf mit --verbose für fehlersuche aufrufen
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // diese Variablen konnte ich NICHT benutzten???
+    var options = {
+        // Project settings
+        paths: {
+           // Configurable paths
+           app: 'app',
+           dist: 'dist'
+        }
+    };
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        pkg_jquery: grunt.file.readJSON('bower_components/jquery/.bower.json'),
+        bower_basepath: 'bower_components',
+
+        // diese Variablen konnte ich benutzten
+        paths: {
+           // Configurable paths
+            app: 'app',
+            dist: 'dist',
+            webjs: 'html/js',
+            webcss: 'html/css',
+            app_ma_icon_02: 'app/ma-icon-02',
+            less_master_base: 'less',
+            bower_font_awesome: '<%= bower_basepath %>/font-awesome',
+            bower_jquery_dist: '<%= bower_basepath %>/jquery/dist',
+            bower_jquery_1_dist: '<%= bower_basepath %>/jquery-legacy/dist'
+        },
+
+        clean  : {
+            dist : {
+                files : [{
+                    dot : true,
+                    src : ['.tmp', '<%= paths.dist %>/*', '!<%= paths.dist %>/.git*']
+                }]
+            }
+        },
+
+        concat: {
+            dist: {
+                src: ['bower_components/jquery/dist/jquery.js'],
+                dest: '<%= paths.dist %>/js/jquery.js'
+            },
+            ma_js: {
+                //src: ['bower_components/jquery/dist/jquery.js'],
+                src: ['<%= paths.app %>/ma/js/ub-man.js',
+                      '<%= paths.app %>/ma/js/cookie_notice.js'
+                    ],
+                dest: '<%= paths.dist %>/js/ub-man.js',
+            },
+        },
+
+        uglify: {
+            dist: {
+                options: {
+                    mangle: false,
+                    compress: true,
+                    sourceMap: true,
+                    preserveComments: false,
+                    banner: "/*! jQuery v<%= pkg_jquery.version %> | " +
+                            "(c) jQuery Foundation | jquery.org/license */"
+                }
+            },
+            ma_js: {
+                options: {
+                    mangle: false,
+                    compress: true,
+                    sourceMap: true,
+                    preserveComments: false,
+                    banner: "/*! ub-man.js | (c) UB Mannheim <%= grunt.template.today('yyyy.mm.dd HH:MM:ss') %> */"
+                },
+                files: {
+                    '<%= paths.webjs %>/ub-man.min.js': '<%= paths.dist %>/js/ub-man.js'
+                },
+            },
+            jquery_1: {
+                options: {
+                    mangle: false,
+                    compress: true,
+                    sourceMap: true,
+                    preserveComments: false,
+                    banner: "/*! jQuery v1.12.2 (<%= grunt.template.today('yyyy.mm.dd HH:MM:ss') %>) | " +
+                            "(c) jQuery Foundation | jquery.org/license */"
+
+                },
+                files: {
+                    '<%= paths.webjs %>/jquery-1.12.2.min.js': '<%= paths.bower_jquery_1_dist %>/jquery.js'
+                },
+            }
+        },
+
+
+        copy : {
+            dist : {
+                files : [ {
+                    expand : true,
+                    dot    : false,
+                    cwd    : '<%= paths.app %>',
+                    dest   : '<%= paths.dist %>',
+                    src    : [ '*.{ico,png,txt}', '.htaccess', 'images/{,*/}*.webp', '{,*/}*.html','styles/fonts/{,*/}*.*' ]
+                }]
+            },
+            styles : {
+                expand : true,
+                dot    : false,
+                cwd    : '<%= paths.app %>/styles',
+                dest   : '.tmp/styles/',
+                src    : '{,*/}*.css'
+            },
+            ma_icon_02_dist : {
+                files : {
+                    '<%= paths.dist %>/css/ub_ma_icon.css' : '<%= paths.app_ma_icon_02 %>/styles.css'
+                }
+            },
+            dist_jquery : {
+                files : {
+                    '<%= paths.webjs %>/jquery.js' : '<%= paths.bower_jquery_dist %>/jquery.js',
+                    '<%= paths.webjs %>/jquery.min.js' : '<%= paths.bower_jquery_dist %>/jquery.min.js',
+                    '<%= paths.webjs %>/jquery.min.map' : '<%= paths.bower_jquery_dist %>/jquery.min.map'
+                }
+            },
+            ma_icon_02_resource : {
+                files : {
+                    '<%= paths.webcss %>/fonts/ma-icon-02.eot' : '<%= paths.app_ma_icon_02 %>/fonts/ma-icon-02.eot',
+                    '<%= paths.webcss %>/fonts/ma-icon-02.svg' : '<%= paths.app_ma_icon_02 %>/fonts/ma-icon-02.svg',
+                    '<%= paths.webcss %>/fonts/ma-icon-02.ttf' : '<%= paths.app_ma_icon_02 %>/fonts/ma-icon-02.ttf',
+                    '<%= paths.webcss %>/fonts/ma-icon-02.woff' : '<%= paths.app_ma_icon_02 %>/fonts/ma-icon-02.woff'
+                }
+            },
+            ma_icon_02_dist_min_to_resource : {
+                files : {
+                    '<%= paths.webcss %>/ub_ma_icon.min.css' : '<%= paths.dist %>/css/ub_ma_icon.min.css',
+                    '<%= paths.webcss %>/ub_ma_icon.min.css.map' : '<%= paths.dist %>/css/ub_ma_icon.min.css.map'
+                }
+            },
+            font_awesome_bootstrap_in_css: {
+                files : {
+                    '<%= paths.webcss %>/Font-Awesome/css/font-awesome.min.css' : '<%= paths.bower_font_awesome %>/css/font-awesome.min.css',
+                    '<%= paths.webcss %>/Font-Awesome/css/font-awesome.css.map' : '<%= paths.bower_font_awesome %>/css/font-awesome.css.map',
+                    '<%= paths.webcss %>/Font-Awesome/css/font-awesome.css' : '<%= paths.bower_font_awesome %>/css/font-awesome.css',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/fontawesome-webfont.woff2' : '<%= paths.bower_font_awesome %>/fonts/fontawesome-webfont.woff2',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/fontawesome-webfont.woff' : '<%= paths.bower_font_awesome %>/fonts/fontawesome-webfont.woff',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/fontawesome-webfont.ttf' : '<%= paths.bower_font_awesome %>/fonts/fontawesome-webfont.ttf',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/fontawesome-webfont.svg' : '<%= paths.bower_font_awesome %>/fonts/fontawesome-webfont.svg',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/fontawesome-webfont.eot' : '<%= paths.bower_font_awesome %>/fonts/fontawesome-webfont.eot',
+                    '<%= paths.webcss %>/Font-Awesome/fonts/FontAwesome.otf' : '<%= paths.bower_font_awesome %>/fonts/FontAwesome.otf'
+                }
+            }
+        },
+
+        cssmin: {
+          options: {
+            compatibility: 'ie8',
+            keepSpecialComments: '*',
+            noAdvanced: true,
+            sourceMap: true
+          },
+          css_core: {
+            sourceMap: true,
+            files: {
+                '<%= paths.webcss %>/booklist.min.css': '<%= paths.dist %>/css/booklist.css'
+            }
+          },
+          mafont: {
+            sourceMap: true,
+            files: {
+                '<%= paths.dist %>/css/ub_ma_icon.min.css': '<%= paths.dist %>/css/ub_ma_icon.css'
+            }
+          }
+        },
+
+        less: {
+          maCompileBase : {
+            options: {
+              strictMath: true,
+              sourceMap: true,
+              outputSourceFiles: true,
+              paths: '<%= paths.app %>/less'
+            },
+            // gilt nicht hier! Vor dem kompilieren wurde die Version aus relaunch_2016 hierher kopiert
+            files: {
+                '<%= paths.dist %>/css/booklist.css': '<%= paths.app %>/less/booklist.less'
+            }
+          }
+        },
+
+        autoprefixer: {
+          options: {
+            browsers: [
+              'Android 2.3',
+              'Android >= 4',
+              'Chrome >= 20',
+              'Firefox >= 24', // Firefox 24 is the latest ESR
+              'Explorer >= 8',
+              'iOS >= 6',
+              'Opera >= 12',
+              'Safari >= 6'
+            ]
+          },
+          css_core: {
+            options: {
+              map: true
+            },
+            src: '<%= paths.dist %>/css/booklist.css'
+          }
+        }
+
+    });
+
+    // werden ersetzt durch require('load-grunt-tasks')(grunt); siehe oben
+    //grunt.loadNpmTasks('grunt-bower-task');
+    //grunt.loadNpmTasks('grunt-bower-concat');
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'concat:dist',
+        'uglify:dist',
+        'copy:dist'
+    ]);
+
+    // Default
+    grunt.registerTask('default', ['bsdist-css']);
+
+    grunt.registerTask('bsdist-css', [
+        'clean:dist',
+        'copy:font_awesome_bootstrap_in_css',
+        'less:maCompileBase',
+        'autoprefixer:css_core',
+        'cssmin:css_core',
+        'copy:dist_jquery',
+        'concat:ma_js',
+        'uglify:ma_js',
+        'uglify:jquery_1'
+    ]);
+
+    grunt.registerTask('ma-font', [
+        'copy:ma_icon_02_dist',
+        'copy:ma_icon_02_resource',
+        'cssmin:mafont',
+        'copy:ma_icon_02_dist_min_to_resource'
+    ]);
+
+};
