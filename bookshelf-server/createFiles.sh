31f4709d8c9a84ba53a2fb77b91ceaf13f3c4b70
Bernd Fallert <bernd.fallert@bib.uni-mannheim.de>
2021-06-21 15:06:16 +0200

    remove CreateMiniVersion.sh from script

--- bookshelf-server/createFiles.sh
+++ bookshelf-server/createFiles.sh
@@ -2,9 +2,9 @@
 perl CreateQRCodeFuerBuecherregal.pl
 perl CreateGesamtBuecherregal.pl
 
-cd html/js
-
-if [ -f ErzeugeMiniVersion.sh ]
-then
-   source ErzeugeMiniVersion.sh
-fi
+#cd html/js
+#
+#if [ -f ErzeugeMiniVersion.sh ]
+#then
+#   source ErzeugeMiniVersion.sh
+#fi
